42ecdd7d03a13c4389e4bc48da5cb1eaf0f3bc29
Jordan Price <rj_price@hotmail.co.uk>
2026-02-27 10:23:35 +0000

    Simplify version dumping to use standard dicts to fix YAML tag issues

--- bin/dump_software_versions.py
+++ bin/dump_software_versions.py
@@ -2,24 +2,26 @@
 
 import yaml
 import sys
-from collections import OrderedDict
 
 def dump_software_versions(versions_list):
     """
     Merge multiple versions.yml files into a single YAML file.
     """
-    all_versions = OrderedDict()
+    all_versions = {}
     for f in versions_list:
         with open(f, 'r') as yaml_file:
             versions = yaml.safe_load(yaml_file)
             for process, version_info in versions.items():
                 if process not in all_versions:
-                    all_versions[process] = OrderedDict()
+                    all_versions[process] = {}
                 for tool, version in version_info.items():
                     all_versions[process][tool] = version
 
+    # Sort by process name for consistency
+    sorted_versions = {k: all_versions[k] for k in sorted(all_versions)}
+
     with open('software_versions.yml', 'w') as f:
-        yaml.dump(all_versions, f, default_flow_style=False)
+        yaml.dump(sorted_versions, f, default_flow_style=False)
 
 if __name__ == "__main__":
     dump_software_versions(sys.argv[1:])
